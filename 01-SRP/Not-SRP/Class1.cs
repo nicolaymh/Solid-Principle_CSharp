d1e1d112e1585123b795bf4985566677e8bea39b
Nicolay <nicolaymorenoherrera@gmail.com>
2024-04-23 21:20:09 -0500

    Not-SRP

---
+++ 01-SRP/Not-SRP/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SRP.Not_SRP
+{
+    // Defining the Product interface.
+    public interface IProduct
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+
+    // Class that implements the Product interface.
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Class to control the view displayed to the user.
+    public class ProductBloc
+    {
+        public void LoadProduct(int id)
+        {
+            Console.WriteLine($"Product {id}, name: 'OLED Tv'");
+        }
+
+        public void SaveProduct(IProduct product)
+        {
+            Console.WriteLine($"Saving to the database product: {product.Name} with Id: {product.Id}");
+        }
+
+        public void NotifyClients()
+        {
+            Console.WriteLine($"Sending emails to the clients");
+        }
+
+        public void OnAddToCart(int productId)
+        {
+            Console.WriteLine($"Adding to cart {productId}");
+        }
+
+    }
+}
---
+++ 01-SRP/Program.cs
@@ -0,0 +1,11 @@
+﻿using _01_SRP.Not_SRP;
+
+
+// Instanciando Not-SRP
+
+var product = new ProductBloc();
+product.LoadProduct(9);
+product.OnAddToCart(4);
+product.NotifyClients();
+var myProduct = new Product { Id = 1, Name = "Smartphone LG" };
+product.SaveProduct(myProduct);
