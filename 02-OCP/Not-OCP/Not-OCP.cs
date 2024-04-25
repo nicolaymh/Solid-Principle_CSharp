eaa141b0b700116bc5ecbab82859278110790ad2
Nicolay <nicolaymorenoherrera@gmail.com>
2024-04-25 12:42:55 -0500

    Not-OCP

---
+++ 02-OCP/Not-OCP/Not-OCP.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+
+namespace _02_OCP.Not_OCP
+{
+
+    public class TodoService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<IEnumerable<dynamic>> GetTodoItems()
+        {
+            var data = await _httpClient.GetFromJsonAsync<IEnumerable<dynamic>>("https://jsonplaceholder.typicode.com/todos/");
+
+            return data;
+        }
+    }
+
+    public class PostService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<IEnumerable<dynamic>> getPosts()
+        {
+            var data = await _httpClient.GetFromJsonAsync<IEnumerable<dynamic>>("https://jsonplaceholder.typicode.com/posts");
+
+            return data;
+        }
+    }
+
+    public class PhotoService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<IEnumerable<dynamic>> getPhotos()
+        {
+            var data = await _httpClient.GetFromJsonAsync<IEnumerable<dynamic>>("https://jsonplaceholder.typicode.com/photos");
+
+            return data;
+        }
+    }
+}
---
+++ 02-OCP/Program.cs
@@ -0,0 +1,23 @@
+﻿using _02_OCP.Not_OCP;
+
+
+//Instanciando Not_OCP
+var todoService = new TodoService();
+var todos = await todoService.GetTodoItems();
+foreach (var item in todos)
+{
+    Console.WriteLine(item);
+}
+var postService = new PostService();
+var posts = await postService.getPosts();
+foreach (var item in posts)
+{
+    Console.WriteLine(item);
+}
+var photoService = new PhotoService();
+var photos = await photoService.getPhotos();
+foreach (var item in photos)
+{
+    Console.WriteLine(item);
+}
+
